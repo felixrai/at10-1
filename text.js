75bdbaa5fe80328593543447c316a1b374387dbe
felixrai <133838065+felixrai@users.noreply.github.com>
2023-10-16 01:06:21 -0300

    Add files via upload

---
+++ text.js
@@ -0,0 +1,6 @@
+function js_style() {
+    document.getElementById('text').style.fontSize='35px';
+    const cor = document.getElementById('text').style.color = 'orange';
+    const letra = document.getElementById('text').style.fontFamily = 'cursive'
+}
+
